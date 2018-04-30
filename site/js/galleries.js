caf9405b8d85984ed058b52ac9a758c3d79efdb7
Vladimir Kosenko <15101867+vkosenko@users.noreply.github.com>
2018-05-01 01:12:14 +0200

    Updated: styles

---
+++ site/js/galleries.js
@@ -0,0 +1,32 @@
+var galleries = [
+    {
+        name: 'Gemälde',
+        hash: 'gemaelde',
+        thumb: 'gemaelde_1.jpg',
+        gallery: [
+            {thumb: 'gemaelde_1.jpg'},
+            {thumb: 'gemaelde_2.jpg'},
+            {thumb: 'gemaelde_3.jpg'},
+            {thumb: 'gemaelde_4.jpg'},
+            {thumb: 'gemaelde_5.jpg'},
+            {thumb: 'gemaelde_6.jpg'}
+        ]
+    },
+    {
+        name: 'Schaufenster',
+        hash: 'schaufenster',
+        thumb: 'schaufenster1.jpg',
+        gallery: [
+            {thumb: 'schaufenster1.jpg'},
+            {thumb: 'schaufenster2.jpg'},
+            {thumb: 'schaufenster3.jpg'},
+            {thumb: 'schaufenster4.jpg'},
+            {thumb: 'schaufenster5.jpg'},
+            {thumb: 'schaufenster6.jpg'},
+            {thumb: 'schaufenster7.jpg'},
+            {thumb: 'schaufenster8.jpg'},
+            {thumb: 'schaufenster9.jpg'},
+            {thumb: 'schaufenster10.jpg'}
+        ]
+    }
+];
